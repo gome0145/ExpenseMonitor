34ae30f13f2fbe089853f93816eef7593bd75a9a
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-12-04 16:19:19 -0500

    Initial Commit

---
+++ ExpenseMonitor Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseMonitor Watch App
+//
+//  Created by Montserrat Gomez on 2023-12-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseMonitor Watch App/ExpenseMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseMonitorApp.swift
+//  ExpenseMonitor Watch App
+//
+//  Created by Montserrat Gomez on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseMonitor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
